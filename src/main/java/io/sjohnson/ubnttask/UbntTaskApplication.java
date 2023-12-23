1fd71509cd7af9bc2cd6042e3d810fad55fafe66
Sven Johnson <sjohnson@sjohnson.io>
2023-12-23 02:41:46 +0200

    Initial Commit

---
+++ src/main/java/io/sjohnson/ubnttask/UbntTaskApplication.java
@@ -0,0 +1,13 @@
+package io.sjohnson.ubnttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbntTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbntTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/sjohnson/ubnttask/UbntTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sjohnson.ubnttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbntTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
